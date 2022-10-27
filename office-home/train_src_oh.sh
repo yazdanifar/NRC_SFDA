293d715b27c53163f7f0d298355d7bf4c343a734
mryf <->
2022-10-27 12:38:29 +0330

    bash

--- office-home/train_src_oh.sh
+++ office-home/train_src_oh.sh
@@ -1,27 +1,28 @@
-#ours
-python train_src.py --dset p2c --home
+seed=$1
 
-python train_src.py --dset p2r --home
+python train_src.py --dset p2c --home --seed $seed
 
-python train_src.py --dset p2a --home
+python train_src.py --dset p2r --home --seed $seed
 
-python train_src.py --dset a2p --home
+python train_src.py --dset p2a --home --seed $seed
 
-python train_src.py --dset a2r --home
+python train_src.py --dset a2p --home --seed $seed
 
-python train_src.py --dset a2c --home
+python train_src.py --dset a2r --home --seed $seed
 
-python train_src.py --dset r2a --home
+python train_src.py --dset a2c --home --seed $seed
 
-python train_src.py --dset r2p --home
+python train_src.py --dset r2a --home --seed $seed
 
-python train_src.py --dset r2c --home
+python train_src.py --dset r2p --home --seed $seed
 
-python train_src.py --dset c2r --home
+python train_src.py --dset r2c --home --seed $seed
 
-python train_src.py --dset c2a --home
+python train_src.py --dset c2r --home --seed $seed
 
-python train_src.py --dset c2p --home
+python train_src.py --dset c2a --home --seed $seed
+
+python train_src.py --dset c2p --home --seed $seed
 
 
 
--- office-home/train_tar_oh.sh
+++ office-home/train_tar_oh.sh
@@ -1,23 +1,25 @@
-python train_tar.py --home --dset a2r  --K 3 --KK 2 --file target --gpu_id 0
+seed=$1
 
-python train_tar.py --home --dset r2a  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset a2r  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset r2c  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset r2a  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset r2p  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset r2c  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset p2a  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset r2p  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset p2c  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset p2a  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset a2p  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset p2c  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset a2c  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset a2p  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset p2r  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset a2c  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset c2a  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset p2r  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset c2p  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset c2a  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
 
-python train_tar.py --home --dset c2r  --K 3 --KK 2 --file target --gpu_id 0
+python train_tar.py --home --dset c2p  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
+
+python train_tar.py --home --dset c2r  --K 3 --KK 2 --file target --batch_size 32 --seed $seed
