b158a1d05f7084d951110d07d9cc721f497d37b8
mryf <->
2022-10-28 12:16:10 +0330

    run dir rearrangement

--- office-home/train_src_oh.sh
+++ office-home/train_src_oh.sh
@@ -1,28 +1,13 @@
 seed=$1
 
-python train_src.py --dset p2c --home --seed $seed
+python train_src.py --dset a --home --seed $seed
 
-python train_src.py --dset p2r --home --seed $seed
+python train_src.py --dset c --home --seed $seed
 
-python train_src.py --dset p2a --home --seed $seed
+python train_src.py --dset p --home --seed $seed
 
-python train_src.py --dset a2p --home --seed $seed
+python train_src.py --dset r --home --seed $seed
 
-python train_src.py --dset a2r --home --seed $seed
-
-python train_src.py --dset a2c --home --seed $seed
-
-python train_src.py --dset r2a --home --seed $seed
-
-python train_src.py --dset r2p --home --seed $seed
-
-python train_src.py --dset r2c --home --seed $seed
-
-python train_src.py --dset c2r --home --seed $seed
-
-python train_src.py --dset c2a --home --seed $seed
-
-python train_src.py --dset c2p --home --seed $seed
 
 
 
